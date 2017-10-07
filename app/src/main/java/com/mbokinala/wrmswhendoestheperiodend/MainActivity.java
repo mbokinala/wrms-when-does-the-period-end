910a24b61b99c8c3318afbdc4580fc5ae8db79f4
Aruna Jyothi Jonnakuti <ajonnakuti@ajonnakuti-macbookpro.roam.corp.google.com>
2017-10-06 21:47:00 -0700

    Added support files

--- app/src/main/java/com/mbokinala/wrmswhendoestheperiodend/MainActivity.java
+++ app/src/main/java/com/mbokinala/wrmswhendoestheperiodend/MainActivity.java
@@ -1,7 +1,12 @@
 package com.mbokinala.wrmswhendoestheperiodend;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.mbokinala.wrmswhendoestheperiodend.schedule.PeriodInfo;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Handler handler = new Handler();
+        final int delay = 1000; //milliseconds
+
+        final TextView currentPeriodTextView = (TextView) findViewById(R.id.currentPeriodTextView);
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                currentPeriodTextView.setText("It is currently " + PeriodInfo.getPeriod() +  " period.");
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
     }
 }
